23a453bbff3a0452e0611910dbd2458f960aba5b
Antonio <antonio@antonio101.com>
2020-03-11 11:53:59 -0400

    Test EC Curve

--- OpenSSL_Client/main.cpp
+++ OpenSSL_Client/main.cpp
@@ -3,6 +3,7 @@
 #include <openssl/bio.h> /* BasicInput/Output streams */
 #include <openssl/err.h> /* errors */
 #include <openssl/ssl.h> /* core library */
+#include <openssl/ec.h> /* */
 
 const std::string connect_str =
   "localhost:8443";
@@ -20,9 +21,8 @@ static void openssl_initialize(void)
   ERR_get_state();
 }
 
-int main()
+static void testOpenSSLClient(void)
 {
-  openssl_initialize();
   SSL *ssl     = NULL;
   BIO *bio     = NULL;
   BIO *out     = NULL;
@@ -102,5 +102,44 @@ int main()
  end:
   BIO_free_all(bio);
   BIO_free(out);
+}
+
+static void testDerKey(void)
+{
+    EC_KEY *ec_key      = nullptr;
+    uint32_t der_len    = 85;
+
+    // DER ecoded elliptic curve.
+    uint8_t der_buf[der_len] = {
+        0x30, 0x53, 0x02, 0x01, 0x01, 0x04, 0x15, 0x02,
+        0xd3, 0xe9, 0x3a, 0x0c, 0xea, 0xc0, 0x14, 0x75,
+        0xe7, 0xc9, 0xb8, 0x1e, 0x7e, 0xc0, 0x92, 0x31,
+        0x20, 0x28, 0xd5, 0x9a, 0xa0, 0x07, 0x06, 0x05,
+        0x2b, 0x81, 0x04, 0x00, 0x01, 0xa1, 0x2e, 0x03,
+        0x2c, 0x00, 0x04, 0x07, 0xdb, 0x74, 0xe7, 0x95,
+        0x09, 0xb3, 0x8f, 0x27, 0x19, 0x63, 0xb5, 0x52,
+        0x36, 0x6b, 0x93, 0xaa, 0x98, 0x18, 0x4a, 0xc9,
+        0x04, 0x8b, 0xbc, 0xf6, 0x03, 0x08, 0x5d, 0x26,
+        0x84, 0xd2, 0xa5, 0x6c, 0x03, 0x10, 0x09, 0xf4,
+        0xc2, 0x90, 0x00, 0xa9, 0x65
+    };
+
+    const uint8_t *tmp_ptr = der_buf;
+    ec_key = d2i_ECPrivateKey(nullptr, &tmp_ptr, der_len);
+
+    if (ec_key) {
+        std::cout << "EC_KEY Generated!\n";
+        EC_KEY_free(ec_key);
+    } else {
+        std::cout << "BAD EC_KEY!\n";
+    }
+}
+
+int main()
+{
+  openssl_initialize();
+  //testOpenSSLClient();
+  testDerKey();
+  
   return 0;
 }
